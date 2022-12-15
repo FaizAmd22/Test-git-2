dd0877cc349976394492d4a0c65d228a8e2c5fb2
Faizhal Ahmad <ahmadfaizhal8@gmail.com>
2022-12-15 14:47:58 +0700

    Menambahkan list baru

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium mollitia corporis, facilis sit modi enim.</p>
+    <li>
+        <ul>satu</ul>
+        <ul>dua</ul>
+        <ul>tiga</ul>
+    </li>
 </body>
 </html>
